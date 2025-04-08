d1d7bafb98877a0d2e6e23538135752302682c2e
dat <tranquangdat23122004@gmail.com>
2025-04-08 16:28:26 +0700

    commit

--- src/main/java/com/mycompany/commentexample/Main.java
+++ src/main/java/com/mycompany/commentexample/Main.java
@@ -2,26 +2,18 @@ package com.mycompany.commentexample;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import com.mycompany.commentexample.proxies.EmailNotificationProxy;
-import com.mycompany.commentexample.repositories.CommentRepository;
 // import com.mycompany.commentexample.proxies.CommentNotificationProxy;
 // import com.mycompany.commentexample.repositories.CommentRepository;
 import com.mycompany.commentexample.services.CommentService;
-import com.mycompany.commentexample.services.UserService;
 
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
 
         CommentService commentService = context.getBean("commentService", CommentService.class);
-
-
-        Comment c2 = new Comment("Commnet1", "Tran Quang Dat");
-        commentService.sendComment(c2);
-
-        Comment c1 = new Comment("Commnet2", "Tran Quang Dat");
-        commentService.sendComment(c1);
-
+        System.out.println("\n");
+        Comment c = new Comment("hello", "John Doe");
+        commentService.publishComment(c);
         context.close();
     } 
 }
--- src/main/java/com/mycompany/commentexample/ProjectConfiguration.java
+++ src/main/java/com/mycompany/commentexample/ProjectConfiguration.java
@@ -2,18 +2,23 @@ package com.mycompany.commentexample;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Scope;
-
 import com.mycompany.commentexample.proxies.CommentNotificationProxy;
 import com.mycompany.commentexample.proxies.EmailNotificationProxy;
 
 @Configuration
 @ComponentScan(basePackages = { "com.mycompany.commentexample.proxies", "com.mycompany.commentexample.repositories", "com.mycompany.commentexample.services", "com.mycompany.commentexample" })
+@EnableAspectJAutoProxy
 public class ProjectConfiguration {
     @Bean
     @Lazy
     public CommentNotificationProxy email1() {
         return new EmailNotificationProxy();
+    }
+
+    @Bean
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
     }
 }
